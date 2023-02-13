4fc3c2de9debe7cd2fc0a06aa5b60d2fe1821eb2
samdk38 <125244983+samdk38@users.noreply.github.com>
2023-02-13 12:28:09 +0530

    Addition of Two Veriable received from User

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a=0,b=0,add=0;
+	printf("Enter Two Veriable:");
+	scanf("%d %d",&a,&b);
+	add = a+b;
+	printf("Addition of Two Veriable:%d",add);
+	return 0;
+}
